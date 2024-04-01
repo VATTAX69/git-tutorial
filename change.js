e64975ae27dccf79dc3496820d83d9c1782dbd1e
Vatshal Deru <vatshal101@gmail.com>
2024-04-01 21:57:22 +0100

    version 1.3 (amended 1.2)

--- change.js
+++ change.js
@@ -1,3 +1,4 @@
 console.log("version 1")
 console.log("change")
 console.log("test origin/master branch")
+console.log("version 1.3")
--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 console.log("congfig.js")
 console.log("version1")
 console.log("test origin/master branch")
+console.log("version 1.3")
